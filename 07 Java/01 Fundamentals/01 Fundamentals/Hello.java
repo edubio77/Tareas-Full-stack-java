7962766ac1f57cb075d8370e1458865ccc9917df
edubio77 <60243535+edubio77@users.noreply.github.com>
2020-02-14 15:50:57 -0600

    Add files via upload

---
+++ 07 Java/01 Fundamentals/01 Fundamentals/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Eduardo");
+	System.out.println("Tengo 36 años de edad.");
+	System.out.println("Mi ciudad natal es Valparaiso");
+    }
+}
+
+
