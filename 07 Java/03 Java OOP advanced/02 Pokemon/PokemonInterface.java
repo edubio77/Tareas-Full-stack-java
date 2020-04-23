2eddfb0dd7f4dce007bdefca96bb60aab414f6ca
edubio77 <60243535+edubio77@users.noreply.github.com>
2020-04-23 11:42:50 -0400

    Delete PokemonInterface.java

--- 07 Java/03 Java OOP advanced/02 Pokemon/PokemonInterface.java
+++
@@ -1,9 +0,0 @@
-public interface PokemonInterface {
-    // Este método crea y retorna un Pokemon.  
-	Pokemon createPokemon(String name, String type,int health);
-	//Este método devuelve un string con el name, health, y type del Pokemon.
-	void pokemonInfo(Pokemon pokemon);
-	//Lista el nombre de todos los Pokemones que tienes en la Pokedex.
-	void listPokemon();
- }
-//javac PokemonInterface.java
