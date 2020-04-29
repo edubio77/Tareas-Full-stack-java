5e94eb9856c3e4ac5148dc623acb234e4033b57e
edubio77 <60243535+edubio77@users.noreply.github.com>
2020-04-29 17:35:45 -0400

    Add files via upload

--- 08 Java Spring I/01 Spring Fundamentals/03 HolaHumano/src/main/java/com/edubio77/holahumano/HomeController.java
+++ 08 Java Spring I/01 Spring Fundamentals/03 HolaHumano/src/main/java/com/edubio77/holahumano/HomeController.java
@@ -1,20 +1,22 @@
 package com.edubio77.holahumano;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 	public class HomeController {
 	    @RequestMapping("/")
-	    public String index(@RequestParam(value="name", required=false) String searchQuery) {
+	    public String index(@RequestParam(value="nombre", required=false) String searchQuery, Model model) {
 	    	if(searchQuery==null) {
-	    
-	    		return "Hello Human";
+	    		model.addAttribute("nombre", searchQuery);
+	    		return "/index.jsp";
 	    	}
 	    	else {
-
-	    		return "Hello " + searchQuery;	
+	    		model.addAttribute("nombre", searchQuery);
+	    		return "/index.jsp";	
 	    	}
 	        
 	    }
